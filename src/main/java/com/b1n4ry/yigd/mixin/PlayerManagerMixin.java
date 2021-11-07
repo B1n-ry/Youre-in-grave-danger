1d592d0ff0af363c6dd90505df2cb5997c28f69e
Pixel <markus.norlander@gmail.com>
2021-11-07 16:47:43 +0100

    Added soulbound enchantment functionality (not an enchantment by itself, just the logic behind it) and the ability to set enchantments as soulbound enchantments in your config file

--- src/main/java/com/b1n4ry/yigd/mixin/PlayerManagerMixin.java
+++ src/main/java/com/b1n4ry/yigd/mixin/PlayerManagerMixin.java
@@ -1,6 +1,11 @@
 package com.b1n4ry.yigd.mixin;
 
+import com.b1n4ry.yigd.Yigd;
+import com.b1n4ry.yigd.api.YigdApi;
 import com.b1n4ry.yigd.core.PlayerEntityExt;
+import net.minecraft.enchantment.EnchantmentHelper;
+import net.minecraft.entity.EquipmentSlot;
+import net.minecraft.entity.mob.MobEntity;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.ItemStack;
 import net.minecraft.item.Items;
@@ -11,24 +16,31 @@ import net.minecraft.server.world.ServerWorld;
 import net.minecraft.util.collection.DefaultedList;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.GameRules;
+import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
-import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.ModifyArg;
-import org.spongepowered.asm.mixin.injection.ModifyVariable;
+import org.spongepowered.asm.mixin.injection.*;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 import org.spongepowered.asm.mixin.injection.callback.LocalCapture;
+import org.spongepowered.asm.mixin.injection.invoke.arg.Args;
 
+import java.util.List;
+import java.util.Map;
 import java.util.Optional;
+import java.util.UUID;
 
 @Mixin(PlayerManager.class)
 public abstract class PlayerManagerMixin {
 
+    @Shadow @Final private Map<UUID, ServerPlayerEntity> playerMap;
+
     // Will make this work in the future with a custom soulbound enchantment
-    @Inject(method = "respawnPlayer", at = @At(value = "RETURN"))
-    private void equipSoulboundItems(ServerPlayerEntity oldPlayer, boolean dimensionChange, CallbackInfoReturnable<ServerPlayerEntity> callback, BlockPos blockPos, boolean forcedSpawn, ServerWorld oldWorld, Optional optional, ServerPlayerInteractionManager interactionManager, ServerWorld newWorld, ServerPlayerEntity newPlayer) {
+    @ModifyArgs(method = "respawnPlayer", at = @At(value = "INVOKE", target = "Lnet/minecraft/server/PlayerManager;setGameMode(Lnet/minecraft/server/network/ServerPlayerEntity;Lnet/minecraft/server/network/ServerPlayerEntity;Lnet/minecraft/server/world/ServerWorld;)V"))
+    private void equipSoulboundItems(Args args) {
+        ServerPlayerEntity newPlayer = args.get(0);
+        ServerPlayerEntity oldPlayer = args.get(1);
+
         if (oldPlayer.getServerWorld().getGameRules().getBoolean(GameRules.KEEP_INVENTORY)) return;
 
         DefaultedList<ItemStack> soulboundItems = ((PlayerEntityExt)oldPlayer).getSoulboundInventory();
@@ -36,12 +48,32 @@ public abstract class PlayerManagerMixin {
         if (soulboundItems == null) return;
         if (soulboundItems.size() == 0) return;
 
-        for (int i = 0; i < soulboundItems.size(); i++) {
-            ItemStack item = soulboundItems.get(i);
-            System.out.println(item.getItem());
+        List<ItemStack> armorInventory = soulboundItems.subList(36, 40);
+        List<ItemStack> mainInventory = soulboundItems.subList(0, 36);
 
-//            if (item == ItemStack.EMPTY || item.getItem() == Items.AIR) continue;
-//            newPlayer.equip(i, item);
+        for (int i = 0; i < armorInventory.size(); i++) {
+            EquipmentSlot equipmentSlot = MobEntity.getPreferredEquipmentSlot(armorInventory.get(i)); // return EquipmentSlot
+            ItemStack armorItem = armorInventory.get(i);
+
+            if (armorItem == ItemStack.EMPTY || armorItem.getItem() == Items.AIR) continue;
+
+            newPlayer.equipStack(equipmentSlot, armorItem);
+        }
+
+        newPlayer.equipStack(EquipmentSlot.OFFHAND, soulboundItems.get(40));
+
+        for (int i = 0; i < mainInventory.size(); i++) { // Replace main inventory from grave
+            newPlayer.equip(i, mainInventory.get(i));
+        }
+
+        if (soulboundItems.size() > 41) {
+            int inventoryOffset = 41;
+            for (YigdApi yigdApi : Yigd.apiMods) {
+                int inventorySize = yigdApi.getInventorySize(newPlayer);
+
+                yigdApi.setInventory(soulboundItems.subList(inventoryOffset, inventoryOffset + inventorySize), newPlayer);
+                inventoryOffset += inventorySize;
+            }
         }
 
         soulboundItems.clear();
