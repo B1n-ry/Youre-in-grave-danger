b9a4f6d26ec8ce3c534fe7436cbe779d082e4d22
Pixel <markus.norlander@gmail.com>
2021-11-20 10:53:35 +0100

    Replaced 1 deprecated import

--- src/main/java/com/b1n4ry/yigd/client/YigdClient.java
+++ src/main/java/com/b1n4ry/yigd/client/YigdClient.java
@@ -2,13 +2,13 @@ package com.b1n4ry.yigd.client;
 
 import com.b1n4ry.yigd.client.render.GraveBlockEntityRenderer;
 import net.fabricmc.api.ClientModInitializer;
-import net.fabricmc.fabric.api.client.rendereregistry.v1.BlockEntityRendererRegistry;
+import net.fabricmc.fabric.api.client.rendering.v1.BlockEntityRendererRegistry;
 
 import static com.b1n4ry.yigd.Yigd.GRAVE_BLOCK_ENTITY;
 
 public class YigdClient implements ClientModInitializer {
     @Override
     public void onInitializeClient() {
-        BlockEntityRendererRegistry.INSTANCE.register(GRAVE_BLOCK_ENTITY, GraveBlockEntityRenderer::new);
+        BlockEntityRendererRegistry.register(GRAVE_BLOCK_ENTITY, GraveBlockEntityRenderer::new);
     }
 }
