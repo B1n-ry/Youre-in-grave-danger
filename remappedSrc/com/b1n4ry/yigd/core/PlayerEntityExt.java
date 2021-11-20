38ba737253e0a6c56d02c363f40a63597b12dc9b
Pixel <markus.norlander@gmail.com>
2021-11-20 11:47:45 +0100

    Removed unnecessary files

--- remappedSrc/com/b1n4ry/yigd/core/PlayerEntityExt.java
+++
@@ -1,8 +0,0 @@
-package com.b1n4ry.yigd.core;
-
-import net.minecraft.item.ItemStack;
-import net.minecraft.util.collection.DefaultedList;
-
-public interface PlayerEntityExt {
-    DefaultedList<ItemStack> getSoulboundInventory();
-}
