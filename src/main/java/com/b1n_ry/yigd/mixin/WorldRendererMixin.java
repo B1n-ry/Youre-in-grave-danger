d25d6bcfebfd26a1f610198565cce658d93a7eae
Pixel <b1nry.dev@gmail.com>
2022-02-10 18:36:06 +0100

    Fixes graves being completely white with some mod installed (idk which but this solves it)

--- src/main/java/com/b1n_ry/yigd/mixin/WorldRendererMixin.java
+++ src/main/java/com/b1n_ry/yigd/mixin/WorldRendererMixin.java
@@ -1,17 +1,42 @@
 package com.b1n_ry.yigd.mixin;
 
 import com.b1n_ry.yigd.client.render.GraveBlockEntityRenderer;
+import net.minecraft.client.MinecraftClient;
+import net.minecraft.client.gl.ShaderEffect;
+import net.minecraft.client.render.Camera;
+import net.minecraft.client.render.GameRenderer;
+import net.minecraft.client.render.LightmapTextureManager;
 import net.minecraft.client.render.WorldRenderer;
+import net.minecraft.client.util.math.MatrixStack;
+import net.minecraft.util.math.Matrix4f;
+import org.jetbrains.annotations.Nullable;
+import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.ModifyVariable;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(WorldRenderer.class)
 public abstract class WorldRendererMixin {
-    @ModifyVariable(method = "render", at = @At(value = "STORE"), ordinal = 3)
-    private boolean willRenderOutlineShader(boolean bl4) {
-        boolean glowingGrave = GraveBlockEntityRenderer.renderGraveGlowing;
-        GraveBlockEntityRenderer.renderGraveGlowing = false;
-        return glowingGrave || bl4;
+    @Shadow
+    private @Nullable ShaderEffect entityOutlineShader;
+    @Shadow @Final
+    private MinecraftClient client;
+
+    private boolean glowingShaderRendered = false;
+
+    @Inject(method = "render", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/gl/ShaderEffect;render(F)V", ordinal = 0))
+    private void onRenderGlowingShader(MatrixStack matrices, float tickDelta, long limitTime, boolean renderBlockOutline, Camera camera, GameRenderer gameRenderer, LightmapTextureManager lightmapTextureManager, Matrix4f positionMatrix, CallbackInfo ci) {
+        glowingShaderRendered = true;
+    }
+
+    @Inject(method = "render", at = @At("RETURN"))
+    private void renderGlowingShader(MatrixStack matrices, float tickDelta, long limitTime, boolean renderBlockOutline, Camera camera, GameRenderer gameRenderer, LightmapTextureManager lightmapTextureManager, Matrix4f positionMatrix, CallbackInfo ci) {
+        if (this.entityOutlineShader != null && !glowingShaderRendered && GraveBlockEntityRenderer.renderGraveGlowing) {
+            this.entityOutlineShader.render(tickDelta);
+            this.client.getFramebuffer().beginWrite(false);
+        }
+        glowingShaderRendered = false;
     }
 }
