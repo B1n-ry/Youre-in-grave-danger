ea0105583cb3a1e3ad1d2d33298715192a22bfd5
Pixel <b1nry.dev@gmail.com>
2022-04-20 21:46:40 +0200

    Fixed issue where hammers and similar from some mods could break graves

---
+++ src/main/java/com/b1n_ry/yigd/mixin/MagnaToolMixin.java
@@ -0,0 +1,21 @@
+package com.b1n_ry.yigd.mixin;
+
+import com.b1n_ry.yigd.Yigd;
+import draylar.magna.api.MagnaTool;
+import net.minecraft.block.BlockState;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.BlockView;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Pseudo;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Redirect;
+
+@Pseudo
+@Mixin(MagnaTool.class)
+public interface MagnaToolMixin {
+    @Redirect(method = "isBlockValidForBreaking", at = @At(value = "INVOKE", target = "Lnet/minecraft/block/BlockState;getHardness(Lnet/minecraft/world/BlockView;Lnet/minecraft/util/math/BlockPos;)F"))
+    private float getHardness(BlockState state, BlockView blockView, BlockPos blockPos) {
+        if (state.isOf(Yigd.GRAVE_BLOCK)) return -1.0f; // Make hammers and such find graves unbreakable, while they can be broken if configured
+        return state.getHardness(blockView, blockPos);
+    }
+}
