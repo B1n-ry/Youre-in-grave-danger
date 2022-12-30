d975a35fb302dbc55a4b010a6ed503c5c4f577bc
B1n_ry <b1nry.devrgmail.com>
2022-12-30 20:05:45 +0100

    Fixed hydra crash Improved build.gradle file

--- src/main/java/com/b1n_ry/yigd/mixin/TwilightEntityUtilMixin.java
+++ src/main/java/com/b1n_ry/yigd/mixin/TwilightEntityUtilMixin.java
@@ -14,7 +14,7 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 @Pseudo
 @Mixin(targets = "twilightforest.util.EntityUtil")
 public class TwilightEntityUtilMixin {
-    @Inject(method = "canDestroyBlock(Lnet/minecraft/world/World;Lnet/minecraft/util/math/BlockPos;Lnet/minecraft/block/BlockState;Lnet/minecraft/entity/Entity;)Z", at = @At(value = "HEAD", target = "Ltwilightforest/util/EntityUtil;canEntityDestroyBlock(Lnet/minecraft/block/BlockState;Lnet/minecraft/world/BlockView;Lnet/minecraft/util/math/BlockPos;Lnet/minecraft/entity/Entity;)Z"))
+    @Inject(method = "canDestroyBlock(Lnet/minecraft/world/World;Lnet/minecraft/util/math/BlockPos;Lnet/minecraft/block/BlockState;Lnet/minecraft/entity/Entity;)Z", at = @At(value = "HEAD", target = "Ltwilightforest/util/EntityUtil;canEntityDestroyBlock(Lnet/minecraft/block/BlockState;Lnet/minecraft/world/BlockView;Lnet/minecraft/util/math/BlockPos;Lnet/minecraft/entity/Entity;)Z"), cancellable = true)
     private static void destroy(World world, BlockPos pos, BlockState state, Entity entity, CallbackInfoReturnable<Boolean> cir) {
         if (state.isOf(Yigd.GRAVE_BLOCK)) cir.setReturnValue(false);
     }
