a92f056a32a51293320099ee080ca05261c8560a
B1n_ry <b1nry.devrgmail.com>
2023-08-16 13:21:16 +0200

    Now compat mods is not required :)

--- src/main/java/com/b1n_ry/yigd/compat/InvModCompat.java
+++ src/main/java/com/b1n_ry/yigd/compat/InvModCompat.java
@@ -1,5 +1,6 @@
 package com.b1n_ry.yigd.compat;
 
+import net.fabricmc.loader.api.FabricLoader;
 import net.minecraft.nbt.NbtCompound;
 import net.minecraft.server.network.ServerPlayerEntity;
 
@@ -9,12 +10,14 @@ import java.util.List;
 public interface InvModCompat<T> {
     List<InvModCompat<?>> invCompatMods = new ArrayList<>();
     static void initModCompat() {
-        invCompatMods.add(new TrinketsCompat());
-        invCompatMods.add(new InventorioCompat());
-        invCompatMods.add(new TravelersBackpackCompat());
-        invCompatMods.add(new LevelzCompat());
-        invCompatMods.add(new NumismaticOverhaulCompat());
-        invCompatMods.add(new OriginsCompat());
+        FabricLoader loader = FabricLoader.getInstance();
+
+        if (loader.isModLoaded("trinkets")) invCompatMods.add(new TrinketsCompat());
+        if (loader.isModLoaded("inventorio")) invCompatMods.add(new InventorioCompat());
+        if (loader.isModLoaded("travelersbackpack")) invCompatMods.add(new TravelersBackpackCompat());
+        if (loader.isModLoaded("levelz")) invCompatMods.add(new LevelzCompat());
+        if (loader.isModLoaded("numismatic-overhaul")) invCompatMods.add(new NumismaticOverhaulCompat());
+        if (loader.isModLoaded("apoli")) invCompatMods.add(new OriginsCompat());
     }
 
     String getModName();
