1f3a15d616b5e76f3a101cfe025e72a492cc178f
B1n-ry <b1nry.dev@gmail.com>
2024-08-05 12:22:24 +0200

    Forgot a '!'

--- src/main/java/com/b1n_ry/yigd/block/GraveBlock.java
+++ src/main/java/com/b1n_ry/yigd/block/GraveBlock.java
@@ -265,7 +265,7 @@ public class GraveBlock extends BlockWithEntity implements BlockEntityProvider, 
     public float calcBlockBreakingDelta(BlockState state, PlayerEntity player, BlockView world, BlockPos pos) {
         if (world.getBlockEntity(pos) instanceof GraveBlockEntity grave && grave.isUnclaimed()
                 && (!YigdConfig.getConfig().graveConfig.retrieveMethods.onBreak
-                || player.getGameProfile().equals(grave.getGraveSkull()))) {
+                || !player.getGameProfile().equals(grave.getGraveSkull()))) {
             return 0;
         }
         return super.calcBlockBreakingDelta(state, player, world, pos);
