5c7189cea4e0c668d6544c484cfcf9e48ba66018
B1n_ry <b1nry.devrgmail.com>
2024-08-05 11:33:04 +0200

    Don't change trinket drop rules if trinket compat is not enabled

--- src/main/java/com/b1n_ry/yigd/mixin/BeansBackpacksTrinketMixin.java
+++ src/main/java/com/b1n_ry/yigd/mixin/BeansBackpacksTrinketMixin.java
@@ -1,7 +1,6 @@
 package com.b1n_ry.yigd.mixin;
 
 import com.b1n_ry.yigd.config.YigdConfig;
-import com.b1n_ry.yigd.util.DropRule;
 import dev.emi.trinkets.api.SlotReference;
 import dev.emi.trinkets.api.TrinketEnums;
 import net.minecraft.entity.LivingEntity;
@@ -15,9 +14,10 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 @Pseudo
 @Mixin(targets = "com/beansgalaxy/backpacks/compat/TrinketsRegistry$1")
 public class BeansBackpacksTrinketMixin {
-    // TODO: Test if this is still required. Either I forgot to remove it, or it's actually important
     @Inject(method = "getDropRule", at = @At("HEAD"), cancellable = true)
     private void changeDropRule(ItemStack stack, SlotReference slot, LivingEntity entity, CallbackInfoReturnable<TrinketEnums.DropRule> cir) {
+        if (!YigdConfig.getConfig().compatConfig.enableTrinketsCompat) return;
+
         TrinketEnums.DropRule dropRule = switch(YigdConfig.getConfig().compatConfig.defaultBeansBackpacksDropRule) {
             case KEEP -> TrinketEnums.DropRule.KEEP;
             case DESTROY -> TrinketEnums.DropRule.DESTROY;
--- src/main/java/com/b1n_ry/yigd/mixin/TravelersBackpackTrinketMixin.java
+++ src/main/java/com/b1n_ry/yigd/mixin/TravelersBackpackTrinketMixin.java
@@ -2,7 +2,6 @@ package com.b1n_ry.yigd.mixin;
 
 import com.b1n_ry.yigd.config.YigdConfig;
 import com.tiviacz.travelersbackpack.compat.trinkets.TravelersBackpackTrinket;
-import com.tiviacz.travelersbackpack.config.TravelersBackpackConfigData;
 import dev.emi.trinkets.api.SlotReference;
 import dev.emi.trinkets.api.TrinketEnums;
 import net.minecraft.entity.LivingEntity;
@@ -11,7 +10,6 @@ import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Pseudo;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.Redirect;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 @Pseudo
@@ -20,6 +18,8 @@ public class TravelersBackpackTrinketMixin {
     @Inject(method = "getDropRule", at = @At(value = "HEAD"), cancellable = true)
     private void getBackpackDropRule(ItemStack stack, SlotReference slot, LivingEntity entity, CallbackInfoReturnable<TrinketEnums.DropRule> cir) {
         // If trinket compat is not enabled, TB should handle backpack according to its own rules. If it's enabled, we should handle it
+        if (!YigdConfig.getConfig().compatConfig.enableTrinketsCompat) return;
+
         TrinketEnums.DropRule dropRule = switch (YigdConfig.getConfig().compatConfig.defaultTravelersBackpackDropRule) {
             case DESTROY -> TrinketEnums.DropRule.DESTROY;
             case KEEP -> TrinketEnums.DropRule.KEEP;
