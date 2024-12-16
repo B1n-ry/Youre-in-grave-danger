11e8745e1e54a58c53b5db8d98f66f5974a4dad4
Marlester <77095872+marlester-dev@users.noreply.github.com>
2024-12-16 12:27:48 +0100

    rename enableClaimApiCompat to enableProtectionApiCompat

--- src/main/java/com/b1n_ry/yigd/config/YigdConfig.java
+++ src/main/java/com/b1n_ry/yigd/config/YigdConfig.java
@@ -272,16 +272,16 @@ public class YigdConfig implements ConfigData {
                     add(new MapEntry("minecraft:the_end", "minecraft:end_stone"));
                     add(new MapEntry("misc", "minecraft:dirt"));
             }};
-            @Comment("Defines whether the block under grave can be generated in claims where the player can NOT place blocks if the claim api compat is enabled.")
+            @Comment("Defines whether the block under grave can be generated in claims where the player can NOT place blocks if protection api compat is enabled.")
             public boolean generateOnProtectedLand = false;
-            @Comment("Defines whether the block under grave can be generated in claims where the player CAN place blocks if the claim api compat is enabled.")
+            @Comment("Defines whether the block under grave can be generated in claims where the player CAN place blocks if protection api compat is enabled.")
             public boolean generateInOwnClaim = true;
         }
     }
 
     public static class CompatConfig {
         @Comment("Enables compatibility with Common Protection API (\"Claim API\"), if present")
-        public boolean enableClaimApiCompat = true;
+        public boolean enableProtectionApiCompat = true;
         @Comment("Defines the standard drop rule in claims where the player can NOT place blocks. While PUT_IN_GRAVE, other drop rules will be prioritized")
         @ConfigEntry.Gui.EnumHandler(option = ConfigEntry.Gui.EnumHandler.EnumDisplayOption.BUTTON)
         public DropRule standardDropRuleInClaim = DropRule.DROP;
