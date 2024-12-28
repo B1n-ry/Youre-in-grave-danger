c55bbb0eb333dcb90c5e3185391529393ced0c7f
B1n-ry <b1nry.dev@gmail.com>
2024-12-28 12:37:48 +0100

    Copied the code from PR improving land claim compat from 1.20-fabric branch

--- src/main/java/com/b1n_ry/yigd/compat/misc_compat_mods/CommonProtectionApiCompat.java
+++ src/main/java/com/b1n_ry/yigd/compat/misc_compat_mods/CommonProtectionApiCompat.java
@@ -6,17 +6,27 @@ import com.b1n_ry.yigd.events.AllowGraveGenerationEvent;
 import com.b1n_ry.yigd.events.DropRuleEvent;
 import com.b1n_ry.yigd.util.DropRule;
 import com.b1n_ry.yigd.util.GraveOverrideAreas;
+import eu.pb4.common.protection.api.CommonProtection;
 import eu.pb4.common.protection.impl.ProtectionImpl;
+import net.minecraft.server.network.ServerPlayerEntity;
 import net.minecraft.util.math.BlockPos;
 
 public class CommonProtectionApiCompat {
     public static void init() {
-        AllowBlockUnderGraveGenerationEvent.EVENT.register((grave, currentUnder) ->
-                YigdConfig.getConfig().graveConfig.blockUnderGrave.generateOnProtectedLand || !ProtectionImpl.isProtected(grave.getWorld(), grave.getPos().down()));
+        AllowBlockUnderGraveGenerationEvent.EVENT.register((grave, currentUnder) -> {
+            if (CommonProtection.canPlaceBlock(grave.getWorld(), grave.getPos().down(), grave.getOwner().gameProfile(), null)) {
+                return YigdConfig.getConfig().graveConfig.blockUnderGrave.generateInOwnClaim;
+            } else {
+                return YigdConfig.getConfig().graveConfig.blockUnderGrave.generateOnProtectedLand;
+            }
+        });
 
         AllowGraveGenerationEvent.EVENT.register((context, grave) -> {
-            if (ProtectionImpl.isProtected(context.world(), grave.getPos()))
+            if (CommonProtection.canPlaceBlock(context.world(), grave.getPos(), grave.getOwner().gameProfile(), context.player())) {
+                return YigdConfig.getConfig().compatConfig.standardDropRuleInOwnClaim == DropRule.PUT_IN_GRAVE;
+            } else {
                 return YigdConfig.getConfig().compatConfig.standardDropRuleInClaim == DropRule.PUT_IN_GRAVE;
+            }
 
             return true;
         });
@@ -27,7 +37,12 @@ public class CommonProtectionApiCompat {
             if (ProtectionImpl.isProtected(context.world(), BlockPos.ofFloored(context.deathPos())))
                 return YigdConfig.getConfig().compatConfig.standardDropRuleInClaim;
 
-            return GraveOverrideAreas.INSTANCE.defaultDropRule;
+            ServerPlayerEntity player = context.player();
+            if (CommonProtection.canPlaceBlock(context.world(), BlockPos.ofFloored(context.deathPos()), player.getGameProfile(), player)) {
+                return YigdConfig.getConfig().compatConfig.standardDropRuleInOwnClaim;
+            } else {
+                return YigdConfig.getConfig().compatConfig.standardDropRuleInClaim;
+            }
         });
     }
 }
