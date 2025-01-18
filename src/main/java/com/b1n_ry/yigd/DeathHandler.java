7975215df5d4a292bb87a3ecf7b7beea6b0ce929
B1n_ry <b1nry.dev@gmail.com>
2025-01-18 21:46:54 +0100

    Applies loss after checking items required for grave generation

--- src/main/java/com/b1n_ry/yigd/DeathHandler.java
+++ src/main/java/com/b1n_ry/yigd/DeathHandler.java
@@ -53,11 +53,6 @@ public class DeathHandler {
         ExpComponent keepExp = expComponent.getSoulboundExp();
         respawnComponent.setSoulboundExp(keepExp);
 
-
-        if (config.inventoryConfig.itemLoss.enabled) {
-            inventoryComponent.applyLoss();
-        }
-
         Vec3d graveGenerationPos = !config.graveConfig.generateOnLastGroundPos ? pos : ((ServerPlayerEntityImpl) player).youre_in_grave_danger$getLastGroundPos();
         GraveComponent graveComponent = new GraveComponent(player.getGameProfile(), inventoryComponent, expComponent,
                 world, graveGenerationPos.add(0D, .5D, 0D), new TranslatableDeathMessage(deathSource, player), killerId);  // Will keep track of player grave (if enabled)
