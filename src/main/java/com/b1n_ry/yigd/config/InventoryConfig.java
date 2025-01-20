d53fb7089dccd1881b1142590cd252675d100171
B1n_ry <b1nry.dev@gmail.com>
2025-01-20 19:25:04 +0100

    Updated en_us lang file so translations match

--- src/main/java/com/b1n_ry/yigd/config/InventoryConfig.java
+++ src/main/java/com/b1n_ry/yigd/config/InventoryConfig.java
@@ -26,7 +26,7 @@ public class InventoryConfig {
         public int lossRangeFrom = 0;
         public int lossRangeTo = 100;
         public boolean weightedSelection = true;
-        @ConfigEntry.Gui.EnumHandler
+        @ConfigEntry.Gui.EnumHandler(option = ConfigEntry.Gui.EnumHandler.EnumDisplayOption.BUTTON)
         public DropRule lossDropRule = DropRule.DESTROY;
 
         @Comment("Chance of losing an item (iterated over every item picked up by lossRange)")
