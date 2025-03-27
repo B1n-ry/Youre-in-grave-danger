2391c7adcf878cec1a526367ff8af22a8a52b81b
B1n_ry <b1nry.dev@gmail.com>
2025-03-27 17:32:01 +0100

    Have accessories containers marked as updated after all contained items has been updated instead of after each individual item

--- src/main/java/com/b1n_ry/yigd/compat/AccessoriesCompat.java
+++ src/main/java/com/b1n_ry/yigd/compat/AccessoriesCompat.java
@@ -301,8 +301,8 @@ public class AccessoriesCompat implements InvModCompat<Map<String, AccessoriesIn
 
                     cosmeticAccessories.setItem(i, slot.stack.copy());
                     container.renderOptions().set(i, slot.visible);
-                    container.markChanged(false);
                 }
+                container.markChanged(false);
             }
 
             return extraItems;
